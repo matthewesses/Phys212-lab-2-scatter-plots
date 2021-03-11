b5ba2ae66fed5149b55375c95762ef38a180bc16
matthewesses <77927566+matthewesses@users.noreply.github.com>
2021-03-11 17:27:23 -0600

    Update Aarish's data.py

--- Aarish's data.py
+++ Aarish's data.py
@@ -6,12 +6,12 @@ Created on Thu Mar 11 12:33:04 2021
 """
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.array([1.96,1.96,1.96,1.95,1.92,1.89,1.82,1.73,1.67,1.5])
-y = np.array([4.68,4.68,4.68,4.4,4,3.252,2.58,1.9,0.596,0.21])
+x = np.array([1.96,1.96,1.96,1.95,1.92,1.89,1.82,1.73,1.67,1.5])#vAccross
+y = np.array([4.68,4.68,4.68,4.4,4,3.252,2.58,1.9,0.596,0.21])#light intensity
 plt.xlabel("Voltage Actual Readings")#names the axis
 plt.ylabel("Light intensity")#names the axis
 plt.scatter(x, y, color = 'red')
-a= np.array([2.07,2.05,2.01,1.97,1.95,1.92,1.87,1.8,1.7,1.6])
-b= np.array([0.71,0.6,0.47,0.356,0.252,0.165,0.104,0.078,0.078,0.078])
+a= np.array([2.07,2.05,2.01,1.97,1.95,1.92,1.87,1.8,1.7,1.6])#vAccross
+b= np.array([0.71,0.6,0.47,0.356,0.252,0.165,0.104,0.078,0.078,0.078])#light intensity
 plt.scatter(a, b, color = 'green')
 plt.show()
